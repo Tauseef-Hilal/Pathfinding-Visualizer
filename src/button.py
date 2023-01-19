e16cf25cd02ebfd0641d8c39009238653cc85533
Tauseef-Hilal <tantary.tauseef.atwork@gmail.com>
2023-01-19 12:24:32 +0530

    Add Button class and some text

---
+++ src/button.py
@@ -0,0 +1,65 @@
+"""
+    button.py
+    Contains Button class
+"""
+
+import pygame
+
+from src.constants import HEIGHT, WIDTH
+
+
+class Button:
+    """Model a button"""
+
+    def __init__(
+            self,
+            text: str,
+            x: float | str,
+            y: float | str,
+            padding: float = 5,
+            font_size: int = 20,
+            foreground_color: pygame.Color = pygame.Color(0, 0, 0),
+            background_color: pygame.Color = pygame.Color(255, 255, 255)
+    ):
+        self.text = text
+        self.padding = padding
+        self.background_color = background_color
+
+        font = pygame.font.SysFont("Verdana", font_size)
+
+        self.text_surf = font.render(
+            self.text, True, foreground_color
+        )
+        self.text_rect = self.text_surf.get_rect()
+
+        if x == "center":
+            x = (WIDTH - self.text_surf.get_width()) / 2
+
+        if y == "center":
+            y = (HEIGHT - self.text_surf.get_height()) / 2
+
+        self.rect = pygame.Rect(
+            float(x), float(y), self.text_rect.width +
+            padding * 2, self.text_rect.height + padding * 2
+        )
+
+        self.text_rect.topleft = self.rect.x + padding, self.rect.y + padding
+
+    def draw(self, surf: pygame.surface.Surface):
+        action = False
+
+        # Get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # Check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        # Draw button
+        pygame.draw.rect(surf, self.background_color, self.rect)
+
+        text_x, text_y = self.rect.x + self.padding, self.rect.y + self.padding
+        surf.blit(self.text_surf, (text_x, text_y))
+
+        return action
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import sys
 import pygame
 
+from src.button import Button
+
 from .constants import (
+    BLACK,
     WHITE,
+    GREEN,
     WIDTH,
     HEIGHT,
     FPS
@@ -10,11 +14,16 @@ from .constants import (
 from .maze import Maze
 
 pygame.init()
+pygame.font.init()
+
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pathfinder")
 
 CLOCK = pygame.time.Clock()
+
+title_font = pygame.font.SysFont("Verdana", 40, True)
+title_surf = title_font.render("PATHFINDER", True, BLACK)
 
 
 def main() -> None:
@@ -23,6 +32,12 @@ def main() -> None:
     """
 
     maze = Maze(surface=WINDOW)
+    button = Button(
+        "Start", "center", HEIGHT - 80,
+        background_color=pygame.Color(*GREEN),
+        foreground_color=pygame.Color(*WHITE),
+        padding=6, font_size=30
+    )
 
     while True:
         for event in pygame.event.get():
@@ -32,9 +47,10 @@ def main() -> None:
 
         WINDOW.fill(WHITE)
 
+        WINDOW.blit(title_surf, ((WIDTH - title_surf.get_width()) / 2, 20))
         maze.draw()
-        maze.solve()
+        if button.draw(WINDOW):
+            maze.solve()
 
-        pygame.time.delay(5000)
         pygame.display.update()
         CLOCK.tick(FPS)
